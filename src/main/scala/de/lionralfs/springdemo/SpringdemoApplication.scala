e4b7d38692f2e02a7a497ea95e17f71dba573534
Lion Ralfs <lion.ralfs@gmail.com>
2022-10-19 18:32:32 +0200

    custom start/stop

--- src/main/scala/de/lionralfs/springdemo/SpringdemoApplication.scala
+++ src/main/scala/de/lionralfs/springdemo/SpringdemoApplication.scala
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.{
   RestController
 }
 import scala.concurrent.Future
-import scala.concurrent.ExecutionContext
 import scala.concurrent.Await
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
 import co.elastic.apm.attach.ElasticApmAttacher
-import co.elastic.apm.api.Traced
+import co.elastic.apm.api.{ElasticApm, Traced}
+import scala.util.Failure
+import scala.util.Success
 
 @SpringBootApplication
 @RestController
@@ -26,12 +27,24 @@ class SpringdemoApplication {
     Await.result(asyncWork(name), Duration(2, TimeUnit.SECONDS))
   }
 
-  @Traced
-  def asyncWork(name: String): Future[String] = {
+  def asyncWork(name: String): Future[String] = wrap(
     Future {
       Thread.sleep(1000)
       s"Hello $name";
     }
+  )
+
+  // see https://github.com/wellcomecollection/scala-libs/blob/f27de0186e5f08409cfb32b253d90dd3c4c79168/typesafe_app/src/main/scala/weco/Tracing.scala#L85
+  def wrap[T](block: => Future[T]): Future[T] = {
+    val span = ElasticApm.currentTransaction().startSpan().setName("wrapped")
+    block.transform(res => {
+      res match {
+        case Success(_)         =>
+        case Failure(exception) => span.captureException(exception)
+      }
+      span.end()
+      res
+    })
   }
 }
 
