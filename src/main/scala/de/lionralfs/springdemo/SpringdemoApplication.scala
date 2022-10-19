d0b53eb1f36ba47614259b1a34b55f11dc66abea
Lion Ralfs <lion.ralfs@gmail.com>
2022-10-19 17:48:54 +0200

    add elastic-apm example

--- src/main/scala/de/lionralfs/springdemo/SpringdemoApplication.scala
+++ src/main/scala/de/lionralfs/springdemo/SpringdemoApplication.scala
@@ -13,7 +13,8 @@ import scala.concurrent.Await
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
-import io.opentelemetry.instrumentation.annotations.WithSpan;
+import co.elastic.apm.attach.ElasticApmAttacher
+import co.elastic.apm.api.Traced
 
 @SpringBootApplication
 @RestController
@@ -25,7 +26,7 @@ class SpringdemoApplication {
     Await.result(asyncWork(name), Duration(2, TimeUnit.SECONDS))
   }
 
-  @WithSpan
+  @Traced
   def asyncWork(name: String): Future[String] = {
     Future {
       Thread.sleep(1000)
@@ -35,5 +36,6 @@ class SpringdemoApplication {
 }
 
 object SpringdemoApplication extends App {
+  ElasticApmAttacher.attach();
   SpringApplication.run(classOf[SpringdemoApplication], args: _*)
 }
